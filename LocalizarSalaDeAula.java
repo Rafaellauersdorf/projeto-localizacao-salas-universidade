753d1202acd4ed3b52b260dc2b459135c1002ddf
Rafael Lauersdorf <117686048+Rafaellauersdorf@users.noreply.github.com>
2023-03-18 13:55:34 -0300

    Update LocalizarSalaDeAula.java

--- LocalizarSalaDeAula.java
+++ LocalizarSalaDeAula.java
@@ -1,53 +1,87 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.model.GeocodingResult;
 import com.google.maps.model.LatLng;
-import java.util.Scanner;
-import com.google.maps.StaticMapsApi;
-import com.google.maps.StaticMapsRequest;
-import com.google.maps.model.Path;
-import com.google.maps.model.Size;
 
-public class LocalizarSalaDeAula {
+public class LocalizacaoSalas {
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/university";
+    private static final String USER = "root";
+    private static final String PASS = "senha123";
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String[] salas = {"Sala A101", "Sala B203", "Sala C301", "Sala D402", "Sala E501"};
-        String[] predios = {"Prédio A", "Prédio B", "Prédio C", "Prédio D", "Prédio E"};
-        int salaEscolhida;
-        
-        System.out.println("Bem-vindo ao aplicativo de localização de salas de aula!");
-        System.out.println("Digite o número da sala de aula que você está procurando: ");
-        
-        for (int i = 0; i < salas.length; i++) {
-            System.out.println((i+1) + ". " + salas[i] + " (" + predios[i] + ")");
+        Scanner scanner = new Scanner(System.in);
 
-        
-        }
-        
-        salaEscolhida = sc.nextInt();
-        
-        System.out.println("Você está procurando pela sala " + salas[salaEscolhida-1] + " no " + predios[salaEscolhida-1] + ".");
-        System.out.println("O mapa do campus será exibido em breve...");
-        // aqui pode ser incluído código para exibir um mapa do campus com a localização da sala de aula escolhida
+        // Obter a localização atual do usuário
+        double latitudeAtual = obterLatitudeAtual(scanner);
+        double longitudeAtual = obterLongitudeAtual(scanner);
+
+        // Obter a localização da sala de aula desejada
+        String salaDesejada = obterSalaDesejada(scanner);
+        LatLng coordenadasSala = obterCoordenadasSala(salaDesejada);
+
+        // Calcular a distância entre a localização atual e a sala de aula desejada
+        double distancia = calcularDistancia(latitudeAtual, longitudeAtual, coordenadasSala);
+
+        // Exibir a distância para o usuário
+        System.out.println("A sala de aula " + salaDesejada + " está a " + distancia + " metros de distância.");
+
+        scanner.close();
     }
-}
 
+    private static double obterLatitudeAtual(Scanner scanner) {
+        System.out.print("Digite a latitude atual: ");
+        return scanner.nextDouble();
+    }
 
-salaEscolhida = sc.nextInt();
+    private static double obterLongitudeAtual(Scanner scanner) {
+        System.out.print("Digite a longitude atual: ");
+        return scanner.nextDouble();
+    }
 
-String sala = salas[salaEscolhida-1];
-String predio = predios[salaEscolhida-1];
-String endereco = sala + ", " + predio + ", Paraná, Brasil";
+    private static String obterSalaDesejada(Scanner scanner) {
+        System.out.print("Digite o nome da sala de aula desejada: ");
+        return scanner.nextLine();
+    }
 
-GeoApiContext context = new GeoApiContext.Builder()
-    .apiKey("sua_chave_de_api_do_google_maps_aqui")
-    .build();
-GeocodingResult[] results = GeocodingApi.geocode(context, endereco).await();
-LatLng coordenadas = results[0].geometry.location;
+    private static LatLng obterCoordenadasSala(String salaDesejada) {
+        try {
+            // Estabelecer conexão com o banco de dados
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
-System.out.println("Você está procurando pela sala " + sala + " no " + predio + ".");
-System.out.println("As coordenadas geográficas da sala de aula são: " + coordenadas.toString());
+            // Consultar o banco de dados para obter a localização da sala de aula desejada
+            String sql = "SELECT latitude, longitude FROM salas WHERE nome = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, salaDesejada);
+            ResultSet rs = stmt.executeQuery();
 
-System.out.println("O mapa do campus com a localização da sala de aula será exibido em breve...");
+            // Obter as coordenadas da sala de aula desejada
+            LatLng coordenadas = null;
+            if (rs.next()) {
+                double latitude = rs.getDouble("latitude");
+                double longitude = rs.getDouble("longitude");
+                coordenadas = new LatLng(latitude, longitude);
+            }
 
-StaticMapsRequest req = StaticMapsApi
+            // Fechar as conexões com o banco de dados
+            rs.close();
+            stmt.close();
+            conn.close();
+
+            return coordenadas;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static double calcularDistancia(double latitude1, double longitude1, LatLng coordenadas2) {
+        // Calcular a distância entre duas coordenadas geográficas usando a fórmula de Haversine
+        double EARTH_RADIUS = 6371.01; // em quilômetros
